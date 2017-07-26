e86ca07ce0a711ed8f5541f3195555290d3ce6de
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-07-26 11:54:36 +0200

    Input value with refs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import NamesList from './components/NamesList.js';
 import Credit from './components/Credit.js';
+import Search from './components/Search.js';
+import Shortlist from './components/Shortlist.js';
+import 'normalize-css';
 import './App.css';
 
 class App extends Component {
   render() {
     return(
       <div>
-        <NamesList data={this.props.data}/>
-        <Credit/>
+        <Search/>
+        <main>
+          <Shortlist/>
+          <NamesList data={this.props.data}/>
+          <Credit/>
+        </main>
       </div>
     );
   }
---
+++ src/components/Search.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class Search extends Component {
+	filterUpdate() {
+		const val = this.myValue.value;
+	}
+	render() {
+		return (
+			<header>
+				<form>
+					<input 
+						type="text"
+						ref={ (value) => this.myValue = value }
+						placeholder="Type to filter"
+						onChange={this.filterUpdate.bind(this)}
+					/>
+				</form>
+			</header>
+		)
+	}
+}
+
+export default Search;
