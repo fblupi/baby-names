dc00a7dcef10fce65065cd7bf708e1dee97ac0f9
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-07-26 11:15:34 +0200

    Our first component output

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          <li className="boy">Name 1</li>
+          <li className="girl">Name 1</li>
+          <li className="boy">Name 2</li>
+          <li className="girl">Name 2</li>
+          <li className="boy">Name 3</li>
+          <li className="girl">Name 3</li>
+        </ul>
       </div>
     );
   }
