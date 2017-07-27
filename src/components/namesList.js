a7373cf95142907d2bdd6013642af862133158b4
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-07-27 09:12:02 +0200

    Adding the shortlisting functionality

--- src/components/namesList.js
+++ src/components/namesList.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
-export default ({data, filterText}) => {
+export default ({data, filterText, addFavourite}) => {
   const namesList = data
     .filter(name => {
       return name.name.toLowerCase().indexOf(filterText.toLowerCase()) >= 0;
     })
     .map(name => {
       return (
-        <li key={name.id} className={name.sex}>{name.name}</li>
+        <li 
+          key={name.id} 
+          className={name.sex}
+          onClick={() => addFavourite(name.id)}
+        >
+          {name.name}
+        </li>
       );
     });
   return (
